68f5d667e5017d56a90294307b76cd70a5a5f5cf
Darkcode12 <78566603+Darkcode12@users.noreply.github.com>
2026-08-16 14:37:05 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'completados-v7';
+const CACHE = 'completados-v9';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
